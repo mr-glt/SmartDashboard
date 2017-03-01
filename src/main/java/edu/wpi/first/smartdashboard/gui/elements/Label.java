ba351bb709514c35ea160ae112b78f1e21c3c6bb
Charlie Wade <colson72098@gmail.com>
2017-02-28 23:37:22 -0500

    Add font size and style to label

--- src/main/java/edu/wpi/first/smartdashboard/gui/elements/Label.java
+++ src/main/java/edu/wpi/first/smartdashboard/gui/elements/Label.java
@@ -1,10 +1,12 @@
 package edu.wpi.first.smartdashboard.gui.elements;
 
 import edu.wpi.first.smartdashboard.gui.StaticWidget;
+import edu.wpi.first.smartdashboard.properties.IntegerProperty;
 import edu.wpi.first.smartdashboard.properties.MultiProperty;
 import edu.wpi.first.smartdashboard.properties.Property;
 import edu.wpi.first.smartdashboard.properties.StringProperty;
 import java.awt.BorderLayout;
+import java.awt.Font;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 
@@ -16,6 +18,8 @@ public class Label extends StaticWidget {
   public final StringProperty text = new StringProperty(this, "Text", "Label");
   public final MultiProperty horizontal = new MultiProperty(this, "Horizontal Alignment");
   public final MultiProperty vertical = new MultiProperty(this, "Vertical Alignment");
+  public final IntegerProperty textSize = new IntegerProperty(this, "Text Size", 12);
+  public final MultiProperty textStyle = new MultiProperty(this, "Text Style");
 
   private JLabel label;
 
@@ -29,6 +33,11 @@ public class Label extends StaticWidget {
     vertical.add("Center", SwingConstants.CENTER);
     vertical.add("Down", SwingConstants.BOTTOM);
     vertical.setDefault("Center");
+
+    textStyle.add("Bold", Font.BOLD);
+    textStyle.add("Italic", Font.ITALIC);
+    textStyle.add("Regular", Font.PLAIN);
+    textStyle.setDefault("Regular");
   }
 
   @Override
@@ -39,6 +48,7 @@ public class Label extends StaticWidget {
 
     label.setHorizontalAlignment((Integer) horizontal.getValue());
     label.setVerticalAlignment((Integer) vertical.getValue());
+    label.setFont(new Font(Font.SANS_SERIF, (Integer) textStyle.getValue(), textSize.getValue()));
 
     add(label, BorderLayout.CENTER);
   }
@@ -51,6 +61,10 @@ public class Label extends StaticWidget {
       label.setHorizontalAlignment((Integer) horizontal.getValue());
     } else if (property == vertical) {
       label.setVerticalAlignment((Integer) vertical.getValue());
+    } else if (property == textSize) {
+      label.setFont(new Font(Font.SANS_SERIF, (Integer) textStyle.getValue(), textSize.getValue()));
+    } else if (property == textStyle) {
+      label.setFont(new Font(Font.SANS_SERIF, (Integer) textStyle.getValue(), textSize.getValue()));
     }
   }
 }
